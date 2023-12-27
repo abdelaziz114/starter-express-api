aa3c01cb00738c5df8a8a1c9c8b3a28ed1f1d3a3
abdelaziz114 <121435293+abdelaziz114@users.noreply.github.com>
2023-12-27 14:57:24 +0000

    add updates

--- src/Routes/plats.js
+++ src/Routes/plats.js
@@ -1,7 +1,17 @@
 const { Router } = require("express");
+const path = require('path');
+const express = require("express");
+//const PlatRouter = Router()
+const sqlite3 = require('sqlite3').verbose(); // Require SQLite module
+const PlatRouter = express.Router();
 
-const PlatRouter = Router()
 
+// SQLite database setup
+const db = new sqlite3.Database(path.join(__dirname, '../DB', 'test.db'));
+
+
+app=express()
+// Set views directory
 
 
 const data_plats = [
@@ -15,6 +25,50 @@ const data_plats = [
     }
 ]
 
+
+PlatRouter.get('/plat/:name', (req, res) => {
+    console.log("Just got a request!");
+    const { name } = req.params;
+    const resultat = data_plats.find((g) => g.name === name);
+    if (resultat) { /// => resultat != null || resultat != undefined || resultat != ""
+        res.send(resultat);
+    } else {
+        res.send(req.params);
+    }
+});
+
+
+
+
+PlatRouter.get('/plats', (req, res) => {
+    const data = { title: 'Plats Page', plats: data_plats };
+    res.render('plats', data);
+});
+
+
+
+// Route to handle order placement
+app.post('/order', (req, res) => {
+    const { id, count, address } = req.body;
+
+    // TODO: Insert the data into the SQLite database
+    db.run('INSERT INTO orders (id, count, address) VALUES (?, ?, ?)', [id, count, address], (err) => {
+        if (err) {
+            console.error(err.message);
+            res.status(500).send('Internal Server Error');
+        } else {
+            res.status(200).send('Order placed successfully!');
+        }
+    });
+});
+
+
+
+///    plats="../view/plats.html"
+   // res.send(plats);
+
+
+/*
 PlatRouter.get('/plat/:name', (req, res) => {
     console.log("Just got a request!");
     const { name } = req.params;
@@ -34,6 +88,6 @@ PlatRouter.get('/plats', (req, res) => {
     }
     res.send(plats);
 })
-
+*/
 
 module.exports = PlatRouter;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 const express = require('express')
 const app = express()
+const path = require('path');
 const PlatRouter=require("/workspaces/starter-express-api/src/Routes/plats")
 const session =require("express-session");
 
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 
+app.use(session({
+    secret:"oaiaosgjsmklgshgthaoithaotioatoa",
+    resave:false,
+    saveUninitialized:false,
+}))
+
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname,  'view')); // Adjust the path accordingly
+
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.post('/', (req, res) => {
     console.log("Just got a request!");
